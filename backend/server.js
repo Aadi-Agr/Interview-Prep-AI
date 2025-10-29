588b9712a4b0d77050ed659f70dd10a4639eaeb8
Aaditya Agrawal <shivaagrawal2003.gmail.com>
2025-10-29 14:17:52 +0530

    Aaditya

--- backend/server.js
+++ backend/server.js
@@ -4,32 +4,35 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require('./routes/authRoutes')
-const sessionRoutes = require('./routes/sessionRoutes')
-const questionRoutes = require('./routes/questionRoutes');
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
 const { protect } = require("./middlewares/authMiddleware");
-const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
+const {
+  generateInterviewQuestions,
+  generateConceptExplanation,
+} = require("./controllers/aiController");
 
 const app = express();
 
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "*",
+    origin: "https://interview-prep-ai-eight-delta.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-connectDB()
+connectDB();
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use('/api/sessions', sessionRoutes);
-app.use('/api/questions', questionRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
